6029c01dd241b7b2aa9f93acf9aa96f0bb519b7e
joannaberlin <joanna.kot.ok@gmail.com>
2022-10-19 11:24:57 +0200

    Add image selecting functionality to processOrder

--- dist/js/script.js
+++ dist/js/script.js
@@ -87,6 +87,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion() {
       const thisProduct = this;
@@ -136,22 +137,38 @@
       // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log('paramId:', paramId, 'param:', param);
-
+        console.log(thisProduct.imageWrapper);
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log('optionId:', optionId, 'option:', option);
           // check if there is param with a name of paramId in formData and if it includes optionId
+
+          const selectorImage = '.' + paramId + '-' + optionId;
+          console.log('SELECTOR:', selectorImage);
+          const image = thisProduct.imageWrapper.querySelector(selectorImage);
+
+          console.log('DUPA:', image);
+          // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             //check if the option is not default
             if (!option.default) {
               price += option.price;
             }
+            console.log('DUPADUPA:', thisProduct.imageWrapper);
+            //2. check if you found it (not every product has images for option)
+            if (image) {
+              //3. if you found it, check if option is selected. If yes, show image. If no, hide image.
+              image.classList.add(classNames.menuProduct.imageVisible);
+            }
           }
           else {
             if (option.default) {
               price -= option.price;
+            }
+            if (image) {
+              image.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
