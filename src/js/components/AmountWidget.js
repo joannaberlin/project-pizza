a40959ca8fdbf5cc8c83c2cb91421637e80e3fc7
joannaberlin <joanna.kot.ok@gmail.com>
2022-11-12 16:35:27 +0100

    Fix AmountWidget

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -8,8 +8,8 @@ class AmountWidget extends BaseWidget {
     const thisWidget = this;
 
     thisWidget.getElements(element);
+    // thisWidget.renderValue();
     thisWidget.initActions();
-
     // console.log('AmountWidget:', thisWidget);
   }
 
@@ -17,6 +17,7 @@ class AmountWidget extends BaseWidget {
     const thisWidget = this;
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    console.log(thisWidget.dom.input);
     thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
@@ -35,11 +36,11 @@ class AmountWidget extends BaseWidget {
 
   initActions() {
     const thisWidget = this;
-
-    thisWidget.dom.input.addEventListener('change', function() {
-      // thisWidget.setValue(thisWidget.dom.input.value);
-      thisWidget.value = thisWidget.dom.input.value;
-    });
+    // thisWidget.dom.input.addEventListener('change', function() {
+    //   thisWidget.value = thisWidget.dom.input.value;
+    // });
+    console.log(thisWidget.value);
+    thisWidget.value = thisWidget.dom.input.value;
     thisWidget.dom.linkDecrease.addEventListener('click', function(event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
